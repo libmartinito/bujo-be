5ff68d9875e91dc7e97306bee2cbf47abe8edc3e
lib <lib.martinito@pm.me>
2022-09-30 19:55:38 +0800

    added log router to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { router as userRouter } from "./src/routes/user.route.js"
+import { router as logRouter } from "./src/routes/log.route.js"
 
 dotenv.config()
 
@@ -8,6 +9,7 @@ const app = express()
 
 app.use(express.json())
 app.use(userRouter)
+app.use(logRouter)
 
 const port = process.env.port
 
