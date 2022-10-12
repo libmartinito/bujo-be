9632dc46adafee982413c8a08597bda7a9f16db4
lib <lib.martinito@pm.me>
2022-10-12 17:39:49 +0800

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
 import { router as userRouter } from "./src/routes/user.route.js"
 import { router as logRouter } from "./src/routes/log.route.js"
 
@@ -7,6 +8,7 @@ dotenv.config()
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(logRouter)
